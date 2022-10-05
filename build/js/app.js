ea3ea6f03a51afb9b0d153cf624b6a8e905c555c
DFurtune <d.furtune18@gmail.com>
2022-10-05 18:26:52 +0300

    add slider

--- build/js/app.js
+++ build/js/app.js
@@ -1,4 +1,15 @@
 'use strict';
 
-$(document).ready(function () {});
+$(document).ready(function () {
+  $(".multiple-items").slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    variableWidth: true,
+    arrows: false,
+    dots: false
+  });
+});
 //# sourceMappingURL=app.js.map
--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,15 @@
 'use strict'
 $( document ).ready(
 	function() {
+		$(".multiple-items").slick({
+			dots: true,
+			infinite: true,
+			speed: 500,
+			slidesToShow: 2,
+			slidesToScroll: 1,
+			variableWidth: true,
+			arrows: false,
+			dots: false
+		  });
 	}
 );
